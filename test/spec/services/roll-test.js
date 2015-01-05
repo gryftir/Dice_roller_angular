e2f985f602a81ec8fbf740b205a2081d597b0913
Lawrence Siebert <gryftir@gmail.com>
2015-01-04 22:31:45 -0800

    moved custom roll comparer to describe function

--- test/spec/services/roll-test.js
+++ test/spec/services/roll-test.js
@@ -3,6 +3,9 @@
 describe('Service: Roll', function () {
 
   var Roll;
+  var rollCompare = function(a, b) {
+    return (a.number === b.number && a.sizeOfDice === b.sizeOfDice && a.addValue === b.addValue);
+  };
   // load the controller's module
   beforeEach(function() {
     
@@ -29,28 +32,18 @@ describe('Service: Roll', function () {
     expect(Roll.rolls.length).toEqual(0);
   });
   it('the default element should be 1, 6, 0', function() {
-
-    var rollCompare = function(a, b) {
-      return (a.number === b.number && a.sizeOfDice === b.sizeOfDice && a.addValue === b.addValue);
-    };
     jasmine.addCustomEqualityTester(rollCompare);
     Roll.add(1,6,0);
     Roll.add();
     expect(Roll.rolls[0]).toEqual(Roll.rolls[1]);
   });
   it('should be able to create elements that aren\'t the default', function () {
-    var rollCompare = function(a, b) {
-      return (a.number === b.number && a.sizeOfDice === b.sizeOfDice && a.addValue === b.addValue);
-    };
     jasmine.addCustomEqualityTester(rollCompare);
     Roll.add();
     Roll.add(1,7,1);
     expect(Roll.rolls[0]).not.toEqual(Roll.rolls[1]);
   });
   it('allows you to make and access subrollers', function() {
-    var rollCompare = function(a, b) {
-      return (a.number === b.number && a.sizeOfDice === b.sizeOfDice && a.addValue === b.addValue);
-    };
     jasmine.addCustomEqualityTester(rollCompare);
     Roll.add();
     expect(Roll.rolls[0]).toBeDefined();
