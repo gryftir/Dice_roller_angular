061c9195a79cc2d6a4308698ae41eb7d1443e7a0
Lawrence Siebert <gryftir@gmail.com>
2015-01-12 21:50:50 -0800

    refactored tests because of simplification of collapseprovider.js

--- test/spec/services/collapseprovider.js
+++ test/spec/services/collapseprovider.js
@@ -7,7 +7,7 @@ describe('Service: CollapseProvider', function () {
   // instantiate service
   var CollapseProvider;
   beforeEach(function() {
-    var injector = angular.injector(['Collapse']);
+    var injector = angular.injector(['services']);
     CollapseProvider = injector.get('CollapseProvider');
     CollapseProvider.reset();
   });
@@ -19,90 +19,36 @@ describe('Service: CollapseProvider', function () {
   it('should let you add an item and retrieve it', function () {
     expect(CollapseProvider.add('test')).toBeTruthy();
     expect(CollapseProvider.get('test')).toBeTruthy();
-    expect(CollapseProvider.get('test').name()).toEqual('test');
   });
-  it('the functions of the Collapser should all work as expected', function () {
+  it('should be collapsed and not held to start with', function () {
     CollapseProvider.add('test');
-    var c = CollapseProvider.get('test');
-    expect(c.isOpen()).toBe(false);
-    expect(c.isClosed()).toBe(true);
-    expect(c.name()).toEqual('test');
-    expect(c.isOver()).toBe(false);
-    expect(c.isHeld()).toBe(false);
-    c.open();
-    expect(c.isOpen()).toBe(true);
-    c.close();
-    expect(c.isOpen()).toBe(false);
-    c.toggle();
-    expect(c.isOpen()).toBe(true);
-    c.toggle();
-    expect(c.isOpen()).toBe(false);
-    c.open();
-    c.hold();
-    expect(c.isHeld()).toBe(true);
-    c.close();
-    expect(c.isOpen()).toBe(true);
-    c.release();
-    c.close();
-    expect(c.isOpen()).toBe(false);
-    c.holdToggle();
-    c.open();
-    expect(c.isOpen()).toBe(false);
-    expect(c.isHeld()).toBe(true);
-    c.holdToggle();
-    expect(c.isHeld()).toBe(false);
-    c.over();
-    expect(c.isOver()).toBe(true);
-    c.notOver();
-    expect(c.isOver()).toBe(false);
-    c.overToggle();
-    expect(c.isOver()).toBe(true);
-    c.overToggle();
-    expect(c.isOver()).toBe(false);
-    expect(c.name('test2')).toEqual('test2');
-    expect(c.name()).toEqual('test2');
+    expect(CollapseProvider.get('test').holdOpen).toBe(false);
+    expect(CollapseProvider.get('test').Collapsed).toBe(true);
   });
 
-  it('should let you delete an item', function () {
+  it('should let you toggle Collapse when not held', function () {
     CollapseProvider.add('test');
-    expect(CollapseProvider.exists('test')).toBeTruthy();
-    CollapseProvider.remove('test');
-    expect(CollapseProvider.get('test')).toBeFalsy();
-    expect(CollapseProvider.exists('test')).toBeFalsy();
+    CollapseProvider.get('test').collapseToggle();
+    expect(CollapseProvider.get('test').Collapsed).toBe(false);
+    CollapseProvider.get('test').collapseToggle();
+    expect(CollapseProvider.get('test').Collapsed).toBe(true);
   });
 
-  it('a Collapser should have the same name as it\'s key', function() {
-
-  });
-  it('should let you change the name of it', function() {
+  it('should let you toggle holdOpen', function () {
     CollapseProvider.add('test');
-    expect(CollapseProvider.nameChange('test', 'test2')).toBeTruthy();
-    expect(CollapseProvider.nameChange('test3', 'test4')).toBeFalsy();
-    expect(CollapseProvider.get('test')).toBeFalsy();
-    expect(CollapseProvider.get('test2')).toBeTruthy();
-    expect(CollapseProvider.get('test2').name()).toEqual('test2');
+    CollapseProvider.get('test').holdToggle();
+    expect(CollapseProvider.get('test').holdOpen).toBe(true);
+    CollapseProvider.get('test').holdToggle();
+    expect(CollapseProvider.get('test').holdOpen).toBe(false);
   });
-  it('should let you add more than one item', function () {
-    CollapseProvider.add('test1');
-    CollapseProvider.add('test2');
-    expect(CollapseProvider.get('test1')).toBeTruthy();
-    expect(CollapseProvider.get('test2')).toBeTruthy();
-  });
-  it('every should work', function () {
-    CollapseProvider.add('test1');
-    CollapseProvider.add('test2');
-    expect(CollapseProvider.get('test1').isOpen()).toBe(false);
-    expect(CollapseProvider.get('test2').isOpen()).toBe(false);
-    expect(CollapseProvider.openExcept()).toBeTruthy();
-    expect(CollapseProvider.get('test1').isOpen()).toBe(true);
-    expect(CollapseProvider.get('test2').isOpen()).toBe(true);
-    expect(CollapseProvider.closeExcept('test1')).toBeTruthy();
-    expect(CollapseProvider.get('test1').isOpen()).toBe(true);
-    expect(CollapseProvider.get('test2').isOpen()).toBe(false);
-    expect(CollapseProvider.turnOn()).toBeFalsy();
-    expect(CollapseProvider.turnOn('test2')).toBeTruthy();
-    expect(CollapseProvider.get('test1').isOpen()).toBe(false);
-    expect(CollapseProvider.get('test2').isOpen()).toBe(true);
+  it('should not let you toggle closed when held', function () {
+    CollapseProvider.add('test');
+    //open
+    CollapseProvider.get('test').collapseToggle();
+    //held
+    CollapseProvider.get('test').holdToggle();
+    CollapseProvider.get('test').collapseToggle();
+    expect(CollapseProvider.get('test').Collapsed).toBe(false);
   });
 
 });
